f6c05e50024198030cf0202f975ad0fbbffaf211
unknown <minsung.lim@samsung.com>
2023-03-11 00:13:05 +0900

    init

---
+++ src/main/java/com/ms/auth/controller/BaseController.java
@@ -0,0 +1,23 @@
+package com.ms.springcloudclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class BaseController {
+
+    @Value("${cloud.config:}")
+    String cloudConfig;
+
+    public BaseController(@Value("${cloud.config.id:}") String cloudConfigId) {
+        System.out.println("cloudConfigId : " + cloudConfigId);
+    }
+
+    @GetMapping("")
+    public String profiles() {
+        return cloudConfig;
+    }
+}
